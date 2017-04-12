3c8fe0e960797318789e1eadd49df1b6eab21831
Alexander Lerma <alexanderrlerma@gmail.com>
2017-04-12 03:09:53 -0500

    part a done

---
+++ p2/black_scholes.m
@@ -0,0 +1,20 @@
+function [ call, put ] = black_scholes(st, ttm, k, r, sigma)
+%compute option price using black_scholes model
+    dp = compute_d(st, ttm, k, r, sigma, 1);
+    dm = compute_d(st, ttm, k, r, sigma, -1);
+    expon = k * exp(-r * ttm);
+    call = st * cdf(dp) - expon * cdf(dm);
+    put = expon - st + call;
+end
+
+function [d] = compute_d (s, ttm, k, r, sigma, factor)
+%compute d plus (factor = 1) or d minus (factor = -1)
+    computed = (r + factor * (sigma ^ 2) / 2) * ttm;
+    d = (log(s / k) + computed) / (sigma * sqrt(ttm));
+end
+
+function [ result ] = cdf(k)
+    f = @(x) exp(-x.^2 / 2);
+    result = (1 / sqrt(2 * pi)) * integral(f,-Inf, k);
+end
+
